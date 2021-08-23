8ccc083198ba113454fa0345f126190c0ca828a4
ioo1332 <ioo168400@gmail.com>
2021-08-23 16:54:42 +0900

    board

---
+++ nodejs_001/routes/index.js
@@ -0,0 +1,27 @@
+// express frame work 의 객체 선언
+const express = require("express");
+// express framework에서 routing을 수행하기 위한
+// sub객체 선언
+const controller = express.Router();
+
+/* GET home page. */
+controller.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+controller.get("/home", function (req, res, next) {
+  res.send("Hello Korea");
+});
+
+controller.get("/json", function (req, res) {
+  let mData = {
+    name: "홍길동",
+    tel: "010-0000-0000",
+    age: 33,
+  };
+  res.json(mData);
+});
+
+// 다른 js에서 import(require)하여 사용할수 있도록
+// conroller 객체를 내보내기
+module.exports = controller;
